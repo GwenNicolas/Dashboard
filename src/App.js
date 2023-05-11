0ec5cd205f2f4d759875acc0f13c30018c766feb
GwenNicolas <gwenaelle.nicolas2022@campus-eni.fr>
2023-05-11 13:16:03 +0200

    1er commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import identification from "./Identification";
+
+function App() {
+  return(
+      <div>
+        <h1>Bonjour</h1>
+        <p>Bonjour à TOI qui regarde!</p>
+         <identification/>
+      </div>
+  );
+}
+export default App
+
---
+++ src/Identification.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+ function identification(){
+    return(
+        <div>
+            <h1>mon identification</h1>
+            <p>ici se trouvera page de connexion</p>
+
+        </div>
+    );
+}
+export default identification()
